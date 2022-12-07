9119f1cd6a2824e6e616dcfd83d16b812157fbf2
PowerKuu <hkleven6@outlook.com>
2022-12-07 17:25:35 +0100

    update

--- src/universal/manager.ts
+++
@@ -1,9 +0,0 @@
-import $router from "mantle.web/router"
-
-export function ScrollTo(id:string, quary:string|undefined = undefined) {
-    const element = document.getElementById(id)
-    if (!element) return
-    element.scrollIntoView({ behavior: 'smooth', block: 'center'})
-    if (quary) $router.update("/", "?page=" + quary, false)
-    else $router.update("/", "", false)
-}
