844504b5497fbfbbf4118877e99c8a51844f4e53
PowerKuu <hkleven6@outlook.com>
2022-12-11 03:11:06 +0100

    update

---
+++ src/routes/home/solutions/solutions.tsx
@@ -0,0 +1,30 @@
+import styles from "./solutions.module.css"
+import { ActionButton, ReadButton } from "../../../components/buttons/buttons"
+
+import Solution from "../solution/solution"
+
+export default () => {
+    return <div class={styles.solutions}>
+        <h2><span>Våre</span> løsninger</h2>
+
+        <div>
+            <Solution heading="Web utvikling"subheading="responsivt">
+                <p>Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>                
+            </Solution>
+
+            <Solution heading="Markedsføring"subheading="effektivt">
+                <p>Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>                
+            </Solution>
+
+            <Solution heading="Web design"subheading="kreativt">
+                <p>Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>                
+                <ReadButton style="margin-top: 1rem;">Lær mer</ReadButton>
+            </Solution>
+
+            <Solution heading="Analyse" subheading="objektivt">
+                <p>Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora.</p>                
+                <ReadButton style="margin-top: 1rem;">Lær mer</ReadButton>
+            </Solution> 
+        </div>
+    </div>
+}
